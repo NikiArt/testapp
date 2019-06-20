527f49d1d2230e5f51a4cb53e83fc82c4b06aefb
Nikita Boiko <genertyniki@gmail.com>
2019-06-20 10:08:13 +0300

    add basic layouts and classes

---
+++ app/src/main/java/ru/nikitaboiko/testapp/Fragments/GiphyFragment.java
@@ -0,0 +1,22 @@
+package ru.nikitaboiko.testapp.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import ru.nikitaboiko.testapp.R;
+
+
+public class GiphyFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_giphy, container, false);
+    }
+}
---
+++ app/src/main/java/ru/nikitaboiko/testapp/MainActivity.java
@@ -0,0 +1,41 @@
+package ru.nikitaboiko.testapp;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.menu_giphy:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.menu_browser:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
